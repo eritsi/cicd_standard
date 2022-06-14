0052b7d673a2876dac506b5c3acb16b6f99ade2b
eritsi <eritsi@gmail.com>
2022-06-14 15:57:31 +0000

    add bnn simplified

---
+++ BNN/bnn_simple.py
@@ -0,0 +1,529 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# ! pip install chainer==1.24.0
+
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from chainer import Chain, Variable, optimizers
+from chainer.optimizer import WeightDecay
+
+
+# ## get data
+
+class ArtificialData(object):
+    """
+    人口データを作成するクラス
+    """
+
+    def __init__(self, n_samples=100, noise_scale=.1):
+        self.n_samples = n_samples
+        self.noise_scale = noise_scale
+
+    def true_function(self, x):
+        """
+        ノイズのない正しいデータを返す関数
+        :param x:
+        :return:
+        :rtype: np.ndarray
+        """
+        raise NotImplementedError
+
+    def make_x(self):
+        return np.sort(np.random.uniform(-1.5, 1.5, size=self.n_samples)).astype(np.float32).reshape(-1, 1)
+
+    def make_noise(self, x):
+        return np.random.normal(loc=0, scale=self.noise_scale, size=x.shape)
+
+    def generate(self):
+        x = self.make_x()
+        y = self.true_function(x)
+        y += self.make_noise(y)
+        return x, y
+
+
+def func2(x):
+    """
+    人口データの正しい関数その2
+    :param np.ndarray x:
+    :return:
+    :rtype: np.ndarray
+    """
+    return np.sin(5 * x) * np.abs(x)
+
+
+class Art2(ArtificialData):
+    def true_function(self, x):
+        return func2(x)
+
+    def make_x(self):
+        x1 = np.random.uniform(-1.5, -.5, size=int(self.n_samples / 2))
+        x2 = np.random.uniform(.5, 1.5, size=self.n_samples - x1.shape[0])
+        x = np.vstack((x1, x2)).reshape(-1, 1)
+        return np.sort(x)
+
+
+def make_data(size, seed=1):
+    """
+    人工データの作成
+    
+    :param int size: 
+    :param str function_type:
+    :param int seed: 
+    :return: データと正しい関数の集合
+    :rtype: tuple[np.array, np.array, function]
+    """
+    np.random.seed(seed)
+    x, y = Art2(size).generate()
+    return x, y
+
+
+x_test = None
+x_train, y_train = make_data(size=100)
+
+plt.scatter(x_train, y_train)
+
+# ## Linear Deep Learning via Chainer
+
+import chainer.functions as F
+import chainer.links as L
+class BNN(Chain):
+    """
+    ベイジアンニューラルネットの重み学習を行うクラス
+    """
+
+    def __init__(self, input_dim, output_dim,
+                 hidden_dim=512, activate="relu", mask_type="gaussian", prob=.5, lengthscale=10.):
+        """
+        :param int input_dim: 入力層の次元数
+        :param int output_dim: 出力層の次元数
+        :param int hidden_dim: 隠れ層の次元数
+        :param str activate: 活性化関数
+        :param str mask_type: 
+            変数へのマスクの種類を表すstring. 
+            "dropout", "gaussian", Noneのいずれかを指定
+        :param float prob: 
+            dropoutの確率を表すfloat. 
+            0.のときdropoutをしないときに一致します. 
+            [0, 1) の小数
+        :param float lengthscale:
+            初期のネットワーク重みの精度パラメータ. 大きい値になるほど0に近い値を取ります. 
+        """
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.activate_name = activate
+        self.activate = self._get_function(activate)
+        self.mask_type = mask_type
+        self.lengthscale = lengthscale
+
+        super().__init__(
+            l1=L.Linear(input_dim, hidden_dim,
+                        initial_bias=np.random.normal(scale=1. / lengthscale, size=(hidden_dim)),
+                        initialW=np.random.normal(scale=1. / lengthscale, size=(hidden_dim, input_dim))),
+            l2=L.Linear(hidden_dim, hidden_dim,
+                        initial_bias=np.random.normal(scale=1. / lengthscale, size=(hidden_dim)),
+                        initialW=np.random.normal(scale=1. / lengthscale, size=(hidden_dim, hidden_dim))),
+            l3=L.Linear(hidden_dim, hidden_dim,
+                        initial_bias=np.random.normal(scale=1. / lengthscale, size=(hidden_dim)),
+                        initialW=np.random.normal(scale=1. / lengthscale, size=(hidden_dim, hidden_dim))),
+            l4=L.Linear(hidden_dim, output_dim,
+                        initial_bias=np.random.normal(scale=1. / lengthscale, size=(output_dim)),
+                        initialW=np.random.normal(scale=1. / lengthscale, size=(output_dim, output_dim)))
+        )
+
+        self.mask = Mask(name=mask_type, prob=prob)
+
+    def _get_function(self, s):
+        """
+        文字列からそれに対応する関数を取得
+        
+        :param str s: 関数を表す文字列
+        :return: 
+        """
+        if s == "relu":
+            f = F.relu
+        elif s == "sigmoid":
+            f = F.sigmoid
+        elif s == "tanh":
+            f = F.tanh
+        else:
+            print("対応する関数が見つかりません")
+            f = lambda x: x
+        return f
+
+    def __call__(self, x, apply_input=False, apply_hidden=True):
+        """
+        ネットワークの出力を作成
+        
+        :param Variable x: 入力ベクトル
+        :param bool apply_hidden: 
+            隠れ層に対してマスクをかけるかのフラグ. 
+            True のときm `mask` によって生成されたマスクを隠れ層に掛ける
+        :param bool apply_input:
+            入力層に対してマスクをかけるかのフラグ. 
+            True にすると学習が不安定になることが観測されているため, 学習時には False が推奨
+        :return: 出力
+        :rtype: Variable
+        """
+        x1 = self.mask.apply(x, apply_input)
+        h1 = self.activate(self.l1(x1))
+
+        h1 = self.mask.apply(h1, apply_hidden)
+        h2 = self.activate(self.l2(h1))
+
+        h2 = self.mask.apply(h2, apply_hidden)
+        h3 = self.activate(self.l3(h2))
+
+        h3 = self.mask.apply(h3, apply_hidden)
+        h4 = self.l4(h3)
+        return h4
+
+    def __str__(self):
+        """
+        ネットワークの条件をいい感じの文字列で表現する
+        :return: ネットワーク条件の文字
+        :rtype: str
+        """
+        s = "hidden={0.hidden_dim}_activate={0.activate_name}_{0.mask}".format(self)
+        return s
+
+
+# ## Bayes Part
+
+class Mask(object):
+    """
+    入力変数に数値を掛けて摂動を与える関数クラス
+    """
+
+    def __init__(self, name="dropout", prob=.5):
+        """
+        :param str name: マスクに用いる手法の名前. "dropout", "gaussian", None のいずれかを指定
+        :param float prob: マスクの確率
+        """
+
+        self.prob = self._check_prob(prob)
+        self.name = name
+        if name == "dropout":
+            self.mask_generator = self._dropout_mask
+        elif name == "gaussian":
+            self.mask_generator = self._gaussian_mask
+        elif name is None or name.lower() == "none":
+            self.mask_generator = self._none_mask
+        else:
+            raise NameError("name: {name} に該当するmask関数が見当たりません. ".format(**locals()))
+
+    def __repr__(self):
+        s = "maskname={0.name}_prob={0.prob}".format(self)
+        return s
+
+    def _check_prob(self, prob):
+        if prob >= 1.:
+            prob = 1.
+        elif prob < 0:
+            prob = 0
+        return prob
+
+    def _dropout_mask(self, size):
+        z = np.random.binomial(1, self.prob, size=size).astype(np.float32) * self.prob ** -1
+        return z
+
+    def _gaussian_mask(self, size):
+        sigma = self.prob / (1. - self.prob)
+        z = np.random.normal(loc=1., scale=sigma, size=size).astype(np.float32)
+        return z
+
+    def _none_mask(self, size):
+        return np.array([1.] * size).astype(np.float32)
+
+    def _make(self, size):
+        z = self.mask_generator(size)
+        return Variable(np.diag(z))
+
+    def apply(self, h, do_mask=True):
+        """
+        ベクトルにマスクを掛ける関数
+        :param Variable h: マスクされる変数
+        :return: masked variable
+        :rtype: Variable
+        """
+        if do_mask is False:
+            return h
+
+        size = h.shape[1]
+        z = self._make(size)
+        z = F.matmul(h, z)
+        return z
+
+
+class Transformer(object):
+    """
+    変数変換の実行クラス
+    初めて変数が与えられたとき, 変換と同時にスケーリングのパラメータを学習し保存します。
+    二回目以降は、一度目で学習したパラメータを用いて変換を行います。
+    """
+
+    def __init__(self, scaling=False):
+        """
+        コンストラクタ
+        :param bool transform_log: 目的変数をログ変換するかのbool. 
+        :param bool scaling: 
+        """
+        self._is_fitted = False
+        self.scaling = scaling
+        if scaling:
+            self.scaler = StandardScaler()
+
+    def _scaling(self, x):
+        """
+
+        :param np.ndarray x: 変換する変数配列
+        :return: 
+        :rtype: np.ndarray
+        """
+        shape = x.shape
+        if len(shape) == 1:
+            x = x.reshape(-1, 1)
+
+        if self._is_fitted:
+            x = self.scaler.transform(x)
+        else:
+            x = self.scaler.fit_transform(x)
+            self._is_fitted = True
+        x = x.reshape(shape)
+        return x
+
+    def transform(self, x):
+        """
+        目的変数のリストを受け取って変換器を作成し, 変換後の値を返す
+        scaling変換 [-1,+1]
+
+        :param np.ndarray x:
+        :rtype: np.ndarray
+        """
+        x_trains = x[:]
+        if self.scaling:
+            x_trains = self._scaling(x)
+
+        return x_trains
+
+    def inverse_transform(self, x):
+        """
+        変換された値を元の値に逆変換
+        :param x: np.ndarray
+        :return: np.ndarray
+        """
+        x_inv = x[:]
+        if self.scaling:
+            x_inv = self.scaler.inverse_transform(x)
+
+        return x_inv
+
+
+def save_logloss(loss, name, save=True):
+    """
+    loss の epoch による変化を plot して保存.
+    :param list loss: loss が格納されたリスト
+    :param str name: ファイル名
+    :param bool save: 保存するかどうかのフラグ. True のとき name で保存する.
+    :return:
+    """
+    loss = np.array(loss)
+    fig = plt.figure(figsize=(10, 6))
+    ax1 = fig.add_subplot(111)
+    ax1.plot(loss[:, 1], color="C0")
+    ax1.set_yscale("log")
+    if save:
+        fig.savefig("{name}.png".format(**locals()), dpi=150)
+    return
+
+
+# +
+def preprocess(X, y=None):
+    """
+    入力変数の変換
+    :param np.ndarray X:
+    :param np.ndarray y:
+    :return: 変換後の変数のタプル
+    :rtype: tuple of (numpy.ndarray, numpy.ndarray)
+    """
+    x_transformed = x_transformer.transform(X)
+    if y is None:
+        return x_transformed
+
+    y_transformed = y_transformer.transform(y)
+    return x_transformed, y_transformed
+
+def inverse_y_transform(y):
+    """
+    予測値の逆変換
+    :param y: 
+    :return: 
+    """
+    return y_transformer.inverse_transform(y)
+
+def preprocess_array_format(x):
+    """
+     array の shape, 及び type をチェックして chainer に投げられるようにする.
+
+    1. shapeの修正:
+         (n_samples, ) -> (n_samples, 1)
+    2. dtype の修正:
+        int -> np.int32
+        float -> np.float32
+
+    :param np.ndarray x:
+    :return:
+    :rtype: np.ndarray
+    """
+    if len(x.shape) == 1:
+        x = x.reshape(-1, 1)
+
+    if np.issubdtype(x.dtype, np.integer):
+        x = x.astype(np.int32)
+    elif np.issubdtype(x.dtype, np.float):
+        x = x.astype(np.float32)
+    else:
+        x = x.astype(np.float32)
+    return x
+
+
+# +
+def plot_posterior(x_test, x_train=None, y_train=None, n_samples=100):
+    xx = np.linspace(-2.5, 2.5, 200).reshape(-1, 1)
+
+    x_train, y_train = x_transformer.inverse_transform(x_train), inverse_y_transform(y_train)
+    predict_values = posterior(xx, n=n_samples)
+
+    predict_mean = predict_values.mean(axis=0)
+    predict_var = predict_values.var(axis=0)
+    tau = (1. - model.mask.prob) * model.lengthscale ** 2. / (2 * len(x_train) * weight_decay)
+    predict_var += tau ** -1
+
+    fig = plt.figure(figsize=(6, 6))
+    ax1 = fig.add_subplot(111)
+    ax1.plot(x_train[:, 0], y_train[:, 0], "o", markersize=6., color="C0", label="Training Data Points",
+             fillstyle="none")
+
+    for i in range(100):
+        if i == 0:
+            ax1.plot(xx[:, 0], predict_values[i], color="C1", alpha=.1, label="Posterior Samples", linewidth=.5)
+        else:
+            ax1.plot(xx[:, 0], predict_values[i], color="C1", alpha=.1, linewidth=.5)
+
+    ax1.plot(xx[:, 0], predict_mean, "--", color="C1", label="Posterior Mean")
+    ax1.fill_between(xx[:, 0], predict_mean + predict_var, predict_mean - predict_var, color="C1",
+                     label="1 $\sigma$", alpha=.5)
+
+    ax1.set_ylim(-3.2, 3.2)
+    ax1.set_xlim(min(xx), max(xx))
+    ax1.legend(loc=4)
+    return fig, ax1
+
+def posterior(x, n=3):
+    """
+    :param np.ndarray x:
+    :param int n: 
+    :return:
+    :rtype: np.ndarray
+    """
+    x = preprocess_array_format(x)
+    x = preprocess(x)
+    x = Variable(x)
+    pred = [model(x, apply_input=False, apply_hidden=True).data.reshape(-1) for _ in range(n)]
+    pred = inverse_y_transform(pred)
+    pred = np.array(pred)
+    return pred
+
+
+# -
+
+weight_decay = 4 * 10 ** -5
+apply_input = False
+n_epoch=1000
+batch_size=20
+freq_print_loss=10
+freq_plot=50
+n_samples=100
+data_name='art2'
+
+model = BNN(x_train.shape[1], y_train.shape[1], hidden_dim=512, activate="relu", mask_type='dropout', prob=.5,
+                 lengthscale=10.)
+
+output_dir = "data/{data_name}/{model}".format(**locals())
+# 画像の出力先作成
+if os.path.exists(output_dir) is False:
+    os.makedirs(output_dir)
+
+# +
+x_transformer = Transformer(scaling=True)
+y_transformer = Transformer(scaling=True)
+
+X, y = preprocess(x_train, y_train)
+
+N = X.shape[0]
+
+# Variable 型への変換
+X = Variable(preprocess_array_format(X))
+y = Variable(preprocess_array_format(y))
+
+# +
+optimizer = optimizers.Adam()
+optimizer.setup(model)
+optimizer.add_hook(WeightDecay(weight_decay))
+list_loss = []
+
+for e in range(1, n_epoch + 1):
+    perm = np.random.permutation(N)
+    for i in range(0, N, batch_size):
+        idx = perm[i: i + batch_size]
+        _x = X[idx]
+        _y = y[idx]
+        model.zerograds()
+        loss = F.mean_squared_error(model(_x, apply_input=apply_input), _y)
+        loss.backward()
+        optimizer.update()
+
+    l = F.mean_squared_error(model(X, False, False), y).data
+    if e % freq_print_loss == 0:
+        print("epoch: {e}\tloss:{l}".format(**locals()))
+
+    if e % freq_plot == 0:
+        fig, ax = plot_posterior(x_test, X.data, y.data, n_samples=n_samples)
+        ax.set_title("epoch:{0:04d}".format(e))
+        fig.tight_layout()
+        file_path = os.path.join(output_dir, "epoch={e:04d}.png".format(**locals()))
+        fig.savefig(file_path, dpi=150)
+        plt.close("all")
+    list_loss.append([e, l])
+
+save_logloss(list_loss, model.__str__())
+
+# +
+from glob import glob
+import os
+from PIL import Image
+
+def make_anime(files, name='anime'):
+    images = list(map(lambda file : Image.open(file) , files))
+    images[0].save(name+'.gif', save_all=True, \
+        append_images=images[1:], optimize=True, duration=10 , loop=0)
+
+l1_images = glob(os.path.join('./data/art2/hidden=512_activate=relu_maskname=dropout_prob=0.5',  "*.png"))
+make_anime(l1_images, 'anime')
+# -
+
+# <img src='./anime.gif' width=400></img>
